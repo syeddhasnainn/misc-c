e66289c38eaafa49b2c3a44858f96ac68f94eb1a
syeddhasnainn <syedhasnainmurtaza@gmail.com>
2024-05-24 01:13:45 +0100

    symbolic constants

--- practice.c
+++ practice.c
@@ -1,18 +1,14 @@
 #include <stdio.h>
 
+// symbolic constants not variables
+
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 main(){
-    int fahr, celsius;
-    int lower, upper, step;
-
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    fahr = lower;
-    while (fahr <= upper) {
-        celsius = 5 * (fahr-32)/ 9;
-        printf("%d\tf%d\n", fahr, celsius);
-        fahr = fahr + step;
+    int fahr;
+    for(fahr = LOWER; fahr <= UPPER;fahr = fahr +STEP){
+        printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr-32));
     }
-
 }
